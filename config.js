c9d3be60a94fb8e1a1deed2dbec1abc665cc46eb
Daniel Chávez <danielsaul96@gmail.com>
2016-08-11 13:58:08 -0500

    MAYOR refactorización; falta dist y problema con js cleaners

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 module.exports = {
-	sassFiles : 'app/scss/',
-	cssFiles : 'app/css/',
-	jsFiles :'app/js/',
-	fontsFiles : 'app/fonts/',
-	imgFiles : 'app/img/',
-	distFiles : 'dist/',
-	indexFile : 'app/index.html',
+	indexFile : 'index.html',
+	indexMinFile : 'index.min.html',
+	styleFile : 'styles.css',
+	styleMinFile : 'styles.min.css',
+	jsFile : 'scripts.js',
+	jsMinFile : 'scripts.min.js',
+	
+	sassPath : 'scss/',
+	cssPath : 'css/',
+	jsPath :'js/',
+	fontsPath : 'fonts/',
+	imgPath : 'img/',
+	
+	srcPath : 'app/',
+	distPath : 'dist/',
 } 
--- gulpfile.js
+++ gulpfile.js
@@ -14,46 +14,107 @@ var htmlmin = require('gulp-html-minify');
 // Utilidades varias
 var rename = require('gulp-rename');
 var copy = require('gulp-copy');
-
-// Configuraciones
 var run = require('run-sequence');
+var clean = require('gulp-clean');
+
+// Configuraciones: HIC SUNT DRACONES!
+
 var config = require('./config');
 
-var sassdir = config.sassFiles;
-var cssdir = config.cssFiles;
-var jsdir = config.jsFiles;
-var fontsdir = config.fontsFiles;
-var imgdir = config.imgFiles;
+var srcdir = config.srcPath;
+var sassdir = srcdir + config.sassPath;
+var cssdir = srcdir + config.cssPath;
+var jsdir = srcdir + config.jsPath;
+var fontsdir = srcdir + config.fontsPath;
+var imgdir = srcdir + config.imgPath;
 
-var index = config.indexFile;
+
+var styleFile = config.styleFile;
+var styleMinFile = config.styleMinFile;
+var jsFile = config.jsFile;
+var jsMinFile = config.jsMinFile;
+var indexFile = config.indexFile;
+var indexMinFile = config.indexMinFile;
+
+var styleFilePath = cssdir + styleFile;
+var styleMinFilePath = cssdir + styleMinFile;
+var jsFilePath = jsdir + jsFile;
+var jsMinFilePath = jsdir + jsMinFile;
+var indexFilePath = srcdir + indexFile;
+var indexMinFilePath = srcdir + indexMinFile;
 
 var distdir = config.distFiles;
 
 // Tasks-------------------------------------------------->
 
+// Cleaners
+
+gulp.task('style-clean', function() {
+	return gulp.src(styleFilePath, {read: false})
+	.pipe(clean());
+});
+
+gulp.task('styleMin-clean', function() {
+	return gulp.src(styleMinFilePath, {read: false})
+	.pipe(clean());
+});
+
+gulp.task('js-clean', function() {
+	return gulp.src(jsFilePath, {read: false})
+	.pipe(clean());
+});
+
+gulp.task('jsMin-clean', function() {
+	return gulp.src(jsMinFilePath, {read: false})
+	.pipe(clean());
+});
+
+//~ gulp.task('index-clean', function() {
+	//~ return gulp.src(indexFilePath, {read: false})
+	//~ .pipe(clean());
+//~ });
+
+gulp.task('indexMin-clean', function() {
+	return gulp.src(indexMinFilePath, {read: false})
+	.pipe(clean());
+});
+
+gulp.task('clean', function() {
+	run(['style-clean', 'styleMin-clean', 'js-clean', 'jsMin-clean',
+		'indexMin-clean']);
+});
+
+// Builders
+
 gulp.task('sass', function() {
+	run(['style-clean', 'styleMin-clean']);
+	
 	return gulp.src(sassdir + '**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest(cssdir))
-	.pipe(rename('styles.min.css'))
+	.pipe(rename(styleMinFile))
 	.pipe(cssmin())
 	.pipe(gulp.dest(cssdir));
 });
 
-gulp.task('js', function() {
+gulp.task('js', function() { // Paralelo!!!!!
+	run('js-clean', 'jsMin-clean');
+	
 	return gulp.src(jsdir + '**/*.js')
-	.pipe(cat('scripts.js'))
+	.pipe(cat(jsFile))
 	.pipe(gulp.dest(jsdir))
-	.pipe(rename('scripts.min.js'))
+	.pipe(rename(jsMinFile))
 	.pipe(ugly())
 	.pipe(gulp.dest(jsdir));
 });
 
 gulp.task('html', function(){
-	return gulp.src(index)
+	run('indexMin-clean');
+	
+	return gulp.src(indexFilePath)
 	.pipe(htmlmin())
-	.pipe(rename('index.min.html'))
-	.pipe(gulp.dest('app/'));
+	.pipe(rename(indexMinFile))
+	.pipe(gulp.dest(srcdir));
 });
 
 // Watcher
@@ -63,12 +124,31 @@ gulp.task('watch', function() {
 	gulp.watch(index, ['html']);
 });
 
-// Distribute
-gulp.task('dist', function() {
-
-});
-
 // Build
 gulp.task('build', function() {
-	run('sass', 'js', 'html', 'dist');
+	run('sass', 'js', 'html');
 });
+
+// Distribute-------------------------------------------->
+
+//~ gulp.task('dist-html', function() {
+	//~ return gulp.src()
+	//~ .pipe(rename('index.html'))
+	//~ .pipe(gulp.dest(distdir));
+//~ });
+
+//~ gulp.task('dist-js', function() {
+	//~ return gulp.src(jsdir + 'scripts.js')
+	//~ .pipe(gulp.dest(distdir + jsdir));
+//~ });
+
+//~ gulp.task('dist-css', function() {
+	//~ return gulp.src('styles.min.css')
+	//~ .pipe(rename('styles.css'))
+	//~ .pipe(gulp.dest(distdir + cssdir));
+//~ });
+
+//~ gulp.task('dist', function() {
+	//~ run('build',
+	//~ 'dist-html', 'dist-js', 'dist-css');
+//~ });
